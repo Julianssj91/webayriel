5088cac99a69bcdd75003f6f2ad8334191d044f1
Julian Molina <ejmolinax91@gmail.com>
2023-02-20 13:34:48 -0300

    php al form

--- form.php
+++ form.php
@@ -1,20 +1,12 @@
 
 <?php
-$name = $_POST['nombre'];
-$email = $_POST['email'];
-$message = $_POST['mensaje'];
+$to = "ejmolinax91@gmail.com";
+$subject = "Nuevo mensaje desde el formulario de contacto";
+$message = implode("\n\n", $_POST);
+$headers = "From: " . $_POST["name"] . " <" . $_POST["email"] . ">\r\n";
+$headers .= "Reply-To: " . $_POST["email"] . "\r\n";
+$headers .= "X-Mailer: PHP/" . phpversion();
 
-$header .= "Content-Type: text/plain";
+mail($to, $subject, $message, $headers);
 
-$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
-$mensaje .= "Su e-mail es: " .$mail . " \r\n";
-$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
-$mensaje .= "Enviado el " .date('d/m/Y', time());
-
-$para = 'ejmolinax91@gmail.com';
-$asunto = 'Mensaje del sitio';
-
-mail($para, $asunto, utf8_decode($mensaje), $header);
-
-// header('Location:exito.html');
 ?>
