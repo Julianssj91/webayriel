58334c8063154f017e47b1827415af06900b75e0
Julian Molina <ejmolinax91@gmail.com>
2023-02-20 13:28:09 -0300

    php al form

--- form.php
+++ form.php
@@ -11,7 +11,7 @@ $mensaje .= "Su e-mail es: " .$mail . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " .date('d/m/Y', time());
 
-$para = 'ejmolianx91@gmail.com';
+$para = 'ejmolinax91@gmail.com';
 $asunto = 'Mensaje del sitio';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
