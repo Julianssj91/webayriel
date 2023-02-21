5ddd59c33e1d954626a7dda0c3f4bfd9f137623b
Julian Molina <ejmolinax91@gmail.com>
2023-02-21 13:10:54 -0300

    netlify sweet

---
+++ js/exito.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    Swal.fire({
+        title: "Bienvenido a mi Pokedex",
+        text: "Aqui podrás buscar tu Pokemon favorito por nombre o número",
+        imageUrl: "pokeballintro.png",
+        confirmButtonColor: "#FF0000",
+        showConfirmButton: true,
+    });})
