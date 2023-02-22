1868f5061d3fe1b1a063b77843850f4897341cbb
Julian Molina <ejmolinax91@gmail.com>
2023-02-22 18:34:41 -0300

    agregados

--- js/exito.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    Swal.fire({
-        title: "Bienvenido a mi Pokedex",
-        text: "Aqui podrás buscar tu Pokemon favorito por nombre o número",
-        imageUrl: "pokeballintro.png",
-        confirmButtonColor: "#FF0000",
-        showConfirmButton: true,
-    });})
